e0e037ed33138505fa222d26e5d8ca84ffa0a414
schuetzo <schuetzo-martin@web.de>
2017-05-09 10:36:43 +0200

    Move to clean C++14

--- Ogre/src/madginelib.h
+++ Ogre/src/madginelib.h
@@ -8,18 +8,6 @@
 
 #if defined(OgreMadgine_EXPORTS)
 #define OGREMADGINE_EXPORT __declspec(dllexport)
-
-#include <unordered_map>
-namespace std {
-	namespace tr1 {
-		template <class Key>
-		using hash = std::hash<Key>;
-
-		template <class Key, class T>
-		using unordered_map = std::unordered_map<Key, T>;
-	}
-}
-
 #else
 #define OGREMADGINE_EXPORT __declspec(dllimport)
 #endif
@@ -36,6 +24,19 @@ namespace std {
 #include <chrono>
 
 #include "forward.h"
+
+#if !defined(_HAS_TR1_NAMESPACE)
+#include <unordered_map>
+namespace std {
+	namespace tr1 {
+		template <class Key>
+		using hash = std::hash<Key>;
+
+		template <class Key, class T>
+		using unordered_map = std::unordered_map<Key, T>;
+	}
+}
+#endif
 
 
 #include <Ogre.h>
