f1ef86f60c529ab59142894420ae48e43b934e9e
schuetzo <schuetzo-martin@web.de>
2017-06-17 20:58:20 +0200

    Fix export macros

--- base/src/baselib.h
+++ base/src/baselib.h
@@ -3,9 +3,17 @@
 /// @cond
 
 #if defined(Base_EXPORTS)
+#ifdef __GNUC__
+#define MADGINE_BASE_EXPORT //__attribute__((dllexport))
+#else
 #define MADGINE_BASE_EXPORT __declspec(dllexport)
+#endif
+#else
+#ifdef __GNUC__
+#define MADGINE_BASE_EXPORT //__attribute__((dllimport))
 #else
 #define MADGINE_BASE_EXPORT __declspec(dllimport)
+#endif
 #endif
 
 #include "baseforward.h"
--- server/src/serverlib.h
+++ server/src/serverlib.h
@@ -3,9 +3,17 @@
 /// @cond
 
 #if defined(MadgineServer_EXPORTS)
+#ifdef __GNUC__
+#define MADGINE_SERVER_EXPORT //__attribute__((dllexport))
+#else
 #define MADGINE_SERVER_EXPORT __declspec(dllexport)
+#endif
+#else
+#ifdef __GNUC__
+#define MADGINE_SERVER_EXPORT //__attribute__((dllexport))
 #else
 #define MADGINE_SERVER_EXPORT __declspec(dllimport)
+#endif
 #endif
 
 #include "serverforward.h"
