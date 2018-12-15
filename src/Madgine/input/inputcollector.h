0d6bce94ec11dd7be31f72b560c999fa3ed87a37
MadManRises <MadManRises@web.de>
2018-12-15 19:00:21 +0100

    Update

--- src/Madgine/input/inputcollector.h
+++ src/Madgine/input/inputcollector.h
@@ -12,13 +12,13 @@ namespace Engine
 	{
 		using InputCollector = UniqueComponentCollector<InputHandler, Window::Window*>;
 
-#ifndef PLUGIN_BUILD
-		extern template MADGINE_CLIENT_EXPORT struct UniqueComponentCollector<InputHandler, Window::Window*>;
-#endif
-
 		using InputSelector = UniqueComponentSelector<InputHandler, Window::Window*>;
 
 	}
+
+#ifndef PLUGIN_BUILD
+	extern template struct MADGINE_CLIENT_EXPORT UniqueComponentCollector<Input::InputHandler, Window::Window*>;
+#endif
 
 }
 
