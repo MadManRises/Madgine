7a0bb1b49d2b0dfc179b9e424a651a3337db2783
MadManRises <MadManRises@web.de>
2019-08-12 12:51:28 +0200

    Provide operator<< properly

--- src/Interfaces/filesystem/path.h
+++ src/Interfaces/filesystem/path.h
@@ -1,46 +1,47 @@
 #pragma once
 
 namespace Engine {
-	namespace Filesystem {
+namespace Filesystem {
 
-		struct INTERFACES_EXPORT Path {
+    struct INTERFACES_EXPORT Path {
 
-			Path(const std::string &s);
-			Path(const char *s = "");
+        Path(const std::string &s);
+        Path(const char *s = "");
 
-			Path &operator/=(const Path &p);
+        Path &operator/=(const Path &p);
 
-			Path operator/(const Path &p) const
-			{
-				Path p2 = *this;
-				p2 /= p;
-				return p2;
-			}
+        Path operator/(const Path &p) const
+        {
+            Path p2 = *this;
+            p2 /= p;
+            return p2;
+        }
 
-			bool operator==(const Path &other) const;
-			bool operator!=(const Path &other) const;
+        bool operator==(const Path &other) const;
+        bool operator!=(const Path &other) const;
 
-			Path parentPath() const;
-			Path relative(const Path &base) const;
+        Path parentPath() const;
+        Path relative(const Path &base) const;
 
-			Path filename() const;
-			std::string stem() const;
-			std::string extension() const;
+        Path filename() const;
+        std::string stem() const;
+        std::string extension() const;
 
-			bool isAbsolute() const;
-			bool isRelative() const;
-			
-			bool empty() const;			
-			const std::string &str() const;
-			const char *c_str() const;
+        bool isAbsolute() const;
+        bool isRelative() const;
 
-		private:
-			std::string mPath;
-		};
+        bool empty() const;
+        const std::string &str() const;
+        const char *c_str() const;
 
-	}
+    private:
+        std::string mPath;
+    };
+
+    inline std::ostream &operator<<(std::ostream &out, const Path &p)
+    {
+        return out << p.str();
+    }
+
 }
-
-inline std::ostream& operator<<(std::ostream& out, const Engine::Filesystem::Path& p) {
-    return out << p.str();
 }
