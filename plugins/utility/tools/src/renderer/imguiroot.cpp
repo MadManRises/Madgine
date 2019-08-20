1321e9bffd250fe1eedc7c6375d99796316c4fef
MadManRises <MadManRises@web.de>
2019-08-20 18:00:05 +0200

    Remove comment & newline

--- plugins/utility/tools/src/renderer/imguiroot.cpp
+++ plugins/utility/tools/src/renderer/imguiroot.cpp
@@ -109,7 +109,6 @@ namespace Tools {
                     window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
                     window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
-                    //??
                     window_flags |= ImGuiWindowFlags_NoBackground;
 
                     // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
