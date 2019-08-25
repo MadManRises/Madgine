7d5759f859a881e318440846942df6a5cfa27357
MadManRises <MadManRises@web.de>
2019-08-25 15:01:06 +0200

    Fix compiler errors

--- src/Modules/serialize/container/synced.h
+++ src/Modules/serialize/container/synced.h
@@ -1,148 +1,139 @@
 #pragma once
 
-#include "../syncable.h"
+#include "../../generic/noopfunctor.h"
 #include "../serializable.h"
 #include "../streams/bufferedstream.h"
-#include "unithelper.h"
+#include "../syncable.h"
 #include "offset.h"
-#include "../../generic/noopfunctor.h"
+#include "unithelper.h"
 
-namespace Engine
-{
-	namespace Serialize
-	{
+namespace Engine {
+namespace Serialize {
 
-		#define SYNCED(Type, Name)   \
+#define SYNCED(Type, Name)                                                                          \
     ::Engine::Serialize::Synced<::Engine::Serialize::CombinedOffsetPtr<Self, __LINE__>, Type> Name; \
     DEFINE_COMBINED_OFFSET(Name)
 
-		template <typename PtrOffset, class T, typename Observer = NoOpFunctor>
-		class Synced : public Syncable<PtrOffset>, public Serializable<PtrOffset>, public UnitHelper<T>, private Observer
-		{
-		public:
-			template <class... _Ty>
-                    Synced(_Ty &&... args)
-                        :
-				mData(std::forward<_Ty>(args)...)
-			{
-			}
+    template <typename PtrOffset, class T, typename Observer = NoOpFunctor>
+    class Synced : public Syncable<PtrOffset>, public Serializable<PtrOffset>, public UnitHelper<T>, private Observer {
+    public:
+        template <class... _Ty>
+        Synced(_Ty &&... args)
+            : mData(std::forward<_Ty>(args)...)
+        {
+        }
 
-					Observer& observer() {
-                                            return *this;
-					}
+        Observer &observer()
+        {
+            return *this;
+        }
 
-			template <class Ty>
-			void operator =(Ty&& v)
-			{
-				if (mData != v)
-				{
-					T old = mData;
-					mData = std::forward<Ty>(v);
-					notify(old);
-				}
-			}
+        template <class Ty>
+        void operator=(Ty &&v)
+        {
+            if (mData != v) {
+                T old = mData;
+                mData = std::forward<Ty>(v);
+                notify(old);
+            }
+        }
 
-			template <class Ty>
-			void operator +=(Ty&& v)
-			{
-				T old = mData;
-				this->mData += std::forward<Ty>(v);
-				notify(old);
-			}
+        template <class Ty>
+        void operator+=(Ty &&v)
+        {
+            T old = mData;
+            mData += std::forward<Ty>(v);
+            notify(old);
+        }
 
-			template <class Ty>
-			void operator -=(Ty&& v)
-			{
-				T old = mData;
-				this->mData -= std::forward<Ty>(v);
-				notify(old);
-			}
+        template <class Ty>
+        void operator-=(Ty &&v)
+        {
+            T old = mData;
+            mData -= std::forward<Ty>(v);
+            notify(old);
+        }
 
-			T* operator->()
-			{
-				return &mData;
-			}
+        T *operator->()
+        {
+            return &mData;
+        }
 
-			T* ptr()
-			{
-				return &mData;
-			}
+        T *ptr()
+        {
+            return &mData;
+        }
 
-			operator const T &() const
-			{
-				return mData;
-			}
+        operator const T &() const
+        {
+            return mData;
+        }
 
-			void readRequest(BufferedInOutStream& in)
-			{
-				T old = mData;
-				this->read_state(in, this->mData);
-				notify(old);
-			}
+        void readRequest(BufferedInOutStream &in)
+        {
+            T old = mData;
+            this->read_state(in, this->mData);
+            notify(old);
+        }
 
-			void readAction(SerializeInStream& in)
-			{
-				T old = mData;
-				this->read_state(in, this->mData);
-				notify(old);
-			}
+        void readAction(SerializeInStream &in)
+        {
+            T old = mData;
+            this->read_state(in, this->mData);
+            notify(old);
+        }
 
-			void readState(SerializeInStream& in)
-			{
-				T old = mData;
-				this->read_state(in, mData);
-				if (!in.isMaster())
-					this->read_id(in, mData);
-				notify(old);
-			}
+        void readState(SerializeInStream &in)
+        {
+            T old = mData;
+            this->read_state(in, mData);
+            if (!in.isMaster())
+                this->read_id(in, mData);
+            notify(old);
+        }
 
-			void writeState(SerializeOutStream& out) const
-			{
-				this->write_id(out, mData);
-				this->write_state(out, mData);
-			}
+        void writeState(SerializeOutStream &out) const
+        {
+            this->write_id(out, mData);
+            this->write_state(out, mData);
+        }
 
-			void setDataSynced(bool b) 
-			{
-				this->setItemDataSynced(mData, b);
-			}
+        void setDataSynced(bool b)
+        {
+            this->setItemDataSynced(mData, b);
+        }
 
-			void setActive(bool active) 
-			{
-				if (!active)
-				{
-                                _Serializable<PtrOffset>::setActive(active);
-					if (mData != T())
-						mNotifySignal.emit(T(), mData);
-				}
-				this->setItemActive(mData, active);
-				if (active)
-				{
-                                    _Serializable<PtrOffset>::setActive(active);
-					if (mData != T())
-						mNotifySignal.emit(mData, T());
-				}
-			}
+        void setActive(bool active)
+        {
+            if (!active) {
+                Serializable<PtrOffset>::setActive(active);
+                if (mData != T {})
+                    Observer::operator()(T {}, mData);
+            }
+            this->setItemActive(mData, active);
+            if (active) {
+                Serializable<PtrOffset>::setActive(active);
+                if (mData != T {})
+                    Observer::operator()(mData, T {});
+            }
+        }
 
-		protected:
-			void notify(const T& old)
-			{
-				if (!PtrOffset::parent(this) || PtrOffset::parent(this)->isSynced())
-				{
-					for (BufferedOutStream* out : this->getMasterActionMessageTargets())
-					{
-						this->write_state(*out, this->mData);
-						out->endMessage();
-					}
-				}
-				if (this->isActive())
-				{
-                                    Observer::operator()(mData, old);					
-				}
-			}
+    protected:
+        void notify(const T &old)
+        {
+            if (!PtrOffset::parent(this) || PtrOffset::parent(this)->isSynced()) {
+                for (BufferedOutStream *out : this->getMasterActionMessageTargets()) {
+                    this->write_state(*out, mData);
+                    out->endMessage();
+                }
+            }
+            if (this->isActive()) {
+                Observer::operator()(mData, old);
+            }
+        }
 
-		private:
-			T mData;
-		};
-	}
+    private:
+        T mData;
+    };
+}
 }
