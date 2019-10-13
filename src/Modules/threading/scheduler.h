aed94025e36cc6cfb8f896aac4019aae1d3109c9
MadManRises <MadManRises@web.de>
2019-10-13 15:20:41 +0200

    Add default parameter

--- src/Modules/threading/scheduler.h
+++ src/Modules/threading/scheduler.h
@@ -1,26 +1,22 @@
 #pragma once
 
-namespace Engine
-{
-	namespace Threading
-	{
+namespace Engine {
+namespace Threading {
 
-		struct MODULES_EXPORT Scheduler
-		{
-			Scheduler(WorkGroup &group, std::vector<SignalSlot::TaskQueue*> additionalQueues);
+    struct MODULES_EXPORT Scheduler {
+        Scheduler(WorkGroup &group, std::vector<SignalSlot::TaskQueue *> additionalQueues = {});
 
-			int go();
+        int go();
 
-			void singleLoop();
+        void singleLoop();
 
-		private:
-			void schedulerLoop(SignalSlot::TaskQueue *queue);
+    private:
+        void schedulerLoop(SignalSlot::TaskQueue *queue);
 
+    private:
+        WorkGroup &mWorkgroup;
+        std::vector<SignalSlot::TaskQueue *> mAdditionalQueues;
+    };
 
-		private:
-			WorkGroup &mWorkgroup;
-			std::vector<SignalSlot::TaskQueue*> mAdditionalQueues;
-		};
-
-	}
+}
 }
