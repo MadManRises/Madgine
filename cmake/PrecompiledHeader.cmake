e0ca322311c961d6759f9aae9b320d98d04ac08a
MadManRises <MadManRises@users.noreply.github.com>
2020-03-10 02:46:03 +0100

    Disable PCH for Emscripten for now

--- cmake/PrecompiledHeader.cmake
+++ cmake/PrecompiledHeader.cmake
@@ -2,7 +2,7 @@ include (Util)
 
 once()
 
-if (NOT COMMAND target_precompile_headers)
+if (EMSCRIPTEN OR NOT COMMAND target_precompile_headers)
 
 function(target_precompile_headers)
 endfunction(target_precompile_headers)
