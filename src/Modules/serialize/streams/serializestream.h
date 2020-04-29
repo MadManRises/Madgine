88a57c92d07cf6d0b4b74399ba4474c917f1512f
MadManRises <MadManRises@users.noreply.github.com>
2020-04-29 22:52:38 +0200

    Fix

--- src/Modules/serialize/streams/serializestream.h
+++ src/Modules/serialize/streams/serializestream.h
@@ -12,131 +12,6 @@
 namespace Engine {
 namespace Serialize {
 
-    template <typename... Ty>
-    void read(SerializeInStream &in, std::tuple<Ty &...> t, const char *name = nullptr)
-    {
-        in.format().beginCompound(in, name);
-        TupleUnpacker::forEach(t, [&](auto &e) { read(in, e); });
-        in.format().endCompound(in, name);
-    }
-
-    template <typename... Ty>
-    void read(SerializeInStream &in, std::tuple<Ty...> &t, const char *name = nullptr)
-    {
-        in.format().beginCompound(in, name);
-        TupleUnpacker::forEach(t, [&](auto &e) { read(in, e); });
-        in.format().endCompound(in, name);
-    }
-
-    //void read(ValueType &result, const char *name);
-
-    template <typename C, typename Creator = DefaultCreator<>>
-    void readContainer(SerializeInStream &in, C &container, const char *name = nullptr, Creator &&creator = {})
-    {
-        using T = typename C::value_type;
-
-        in.format().beginExtended(in, name);
-        decltype(container.size()) size;
-        read(in, size, "size");
-        in.format().beginCompound(in, name);
-
-        if constexpr (!container_traits<C>::is_fixed_size) {
-            container.clear();
-
-            while (size--) {
-                //TODO use creator
-                UnitHelper<T>::beginExtendedItem(in, nullref<T>);
-                auto pib = container_traits<C>::emplace(container, container.end());
-                assert(pib.second);
-                auto &t = *pib.first;
-                read(in, t, "Item");
-            }
-        } else {
-            assert(container.size() == size);
-            for (T &t : container) {
-                read(in, t, "Item");
-            }
-        }
-
-        in.format().endCompound(in, name);
-    }
-
-    template <typename T, typename... Args>
-    void read(SerializeInStream &in, T &t, const char *name, Args &&... args)
-    {
-        if constexpr (PrimitiveTypesContain_v<T> || std::is_enum_v<T>) {
-            in.format().beginPrimitive(in, name, PrimitiveTypeIndex_v<T>);
-            in.readUnformatted(t);
-            in.format().endPrimitive(in, name, PrimitiveTypeIndex_v<T>);
-            //mLog.log(t);
-        } else if constexpr (std::is_base_of<SerializableBase, T>::value) {
-            t.readState(in, name, std::forward<Args>(args)...);
-        } else if constexpr (std::is_base_of<SerializableUnitBase, T>::value) {
-            t.readState(in, name, StateTransmissionFlags_DontApplyMap);
-        } else if constexpr (is_instance_v<T, std::unique_ptr>) {
-            read(in, *t, name, std::forward<Args>(args)...);
-        } else if constexpr (is_iterable_v<T>) {
-            readContainer(in, t, name, std::forward<Args>(args)...);
-        } else if constexpr (TupleUnpacker::is_tuplefyable_v<T>) {
-            read(in, TupleUnpacker::toTuple(t), name);
-        } else {
-            static_assert(dependent_bool<T, false>::value, "Invalid Type");
-        }
-    }
-
-    template <typename T>
-    void read(SerializeInStream &in, T &t)
-    {
-        read(in, t, nullptr);
-    }
-
-    template <typename T>
-    void write(SerializeOutStream &out, const T &t, const char *name = nullptr)
-    {
-        if constexpr (PrimitiveTypesContain_v<T> || std::is_enum_v<T>) {
-            out.format().beginPrimitive(out, name, PrimitiveTypeIndex_v<T>);
-            out.writeUnformatted(t);
-            out.format().endPrimitive(out, name, PrimitiveTypeIndex_v<T>);
-            //mLog.log(t);
-        } else if constexpr (std::is_base_of<SerializableBase, T>::value || std::is_base_of<SerializableUnitBase, T>::value) {
-            t.writeState(out, name);
-        } else if constexpr (is_instance_v<T, std::unique_ptr>) {
-            write(out, *t, name);
-        } else if constexpr (is_iterable_v<T>) {
-            writeContainer(out, t, name);
-        } else if constexpr (TupleUnpacker::is_tuplefyable_v<T>) {
-            write(out, TupleUnpacker::toTuple(t), name);
-        } else {
-            static_assert(dependent_bool<T, false>::value, "Invalid Type");
-        }
-    }
-
-    template <typename... Ty>
-    void write(SerializeOutStream &out, const std::tuple<Ty...> &t, const char *name = nullptr)
-    {
-        out.format().beginCompound(out, name);
-        TupleUnpacker::forEach(t, [&](const auto &e) { write(out, e, "Element"); });
-        out.format().endCompound(out, name);
-    }
-
-    template <typename C>
-    void writeContainer(SerializeOutStream &out, const C &container, const char *name = nullptr)
-    {
-        using T = typename C::value_type;
-
-        out.format().beginExtended(out, name);
-        write(out, container.size(), "size");
-        out.format().beginCompound(out, name);
-        for (const auto &t : container) {
-            if (UnitHelper<T>::filter(out, t)) {
-                UnitHelper<T>::beginExtendedItem(out, t);
-                UnitHelper<T>::write_creation(out, t);
-                write(out, t, "Item");
-            }
-        }
-        out.format().endCompound(out, name);
-    }
-
     struct MODULES_EXPORT SerializeInStream : InStream {
     public:
         SerializeInStream();
@@ -288,6 +163,133 @@ namespace Serialize {
     protected:
         Debugging::StreamLog mLog;
     };
+
+    
+    template <typename... Ty>
+    void read(SerializeInStream &in, std::tuple<Ty &...> t, const char *name = nullptr)
+    {
+        in.format().beginCompound(in, name);
+        TupleUnpacker::forEach(t, [&](auto &e) { read(in, e); });
+        in.format().endCompound(in, name);
+    }
+
+    template <typename... Ty>
+    void read(SerializeInStream &in, std::tuple<Ty...> &t, const char *name = nullptr)
+    {
+        in.format().beginCompound(in, name);
+        TupleUnpacker::forEach(t, [&](auto &e) { read(in, e); });
+        in.format().endCompound(in, name);
+    }
+
+    //void read(ValueType &result, const char *name);
+
+    template <typename C, typename Creator = DefaultCreator<>>
+    void readContainer(SerializeInStream &in, C &container, const char *name = nullptr, Creator &&creator = {})
+    {
+        using T = typename C::value_type;
+
+        in.format().beginExtended(in, name);
+        decltype(container.size()) size;
+        read(in, size, "size");
+        in.format().beginCompound(in, name);
+
+        if constexpr (!container_traits<C>::is_fixed_size) {
+            container.clear();
+
+            while (size--) {
+                //TODO use creator
+                UnitHelper<T>::beginExtendedItem(in, nullref<T>);
+                auto pib = container_traits<C>::emplace(container, container.end());
+                assert(pib.second);
+                auto &t = *pib.first;
+                read(in, t, "Item");
+            }
+        } else {
+            assert(container.size() == size);
+            for (T &t : container) {
+                read(in, t, "Item");
+            }
+        }
+
+        in.format().endCompound(in, name);
+    }
+
+    template <typename T, typename... Args>
+    void read(SerializeInStream &in, T &t, const char *name, Args &&... args)
+    {
+        if constexpr (PrimitiveTypesContain_v<T> || std::is_enum_v<T>) {
+            in.format().beginPrimitive(in, name, PrimitiveTypeIndex_v<T>);
+            in.readUnformatted(t);
+            in.format().endPrimitive(in, name, PrimitiveTypeIndex_v<T>);
+            //mLog.log(t);
+        } else if constexpr (std::is_base_of<SerializableBase, T>::value) {
+            t.readState(in, name, std::forward<Args>(args)...);
+        } else if constexpr (std::is_base_of<SerializableUnitBase, T>::value) {
+            t.readState(in, name, StateTransmissionFlags_DontApplyMap);
+        } else if constexpr (is_instance_v<T, std::unique_ptr>) {
+            read(in, *t, name, std::forward<Args>(args)...);
+        } else if constexpr (is_iterable_v<T>) {
+            readContainer(in, t, name, std::forward<Args>(args)...);
+        } else if constexpr (TupleUnpacker::is_tuplefyable_v<T>) {
+            read(in, TupleUnpacker::toTuple(t), name);
+        } else {
+            static_assert(dependent_bool<T, false>::value, "Invalid Type");
+        }
+    }
+
+    template <typename T>
+    void read(SerializeInStream &in, T &t)
+    {
+        read(in, t, nullptr);
+    }
+
+    template <typename T>
+    void write(SerializeOutStream &out, const T &t, const char *name = nullptr)
+    {
+        if constexpr (PrimitiveTypesContain_v<T> || std::is_enum_v<T>) {
+            out.format().beginPrimitive(out, name, PrimitiveTypeIndex_v<T>);
+            out.writeUnformatted(t);
+            out.format().endPrimitive(out, name, PrimitiveTypeIndex_v<T>);
+            //mLog.log(t);
+        } else if constexpr (std::is_base_of<SerializableBase, T>::value || std::is_base_of<SerializableUnitBase, T>::value) {
+            t.writeState(out, name);
+        } else if constexpr (is_instance_v<T, std::unique_ptr>) {
+            write(out, *t, name);
+        } else if constexpr (is_iterable_v<T>) {
+            writeContainer(out, t, name);
+        } else if constexpr (TupleUnpacker::is_tuplefyable_v<T>) {
+            write(out, TupleUnpacker::toTuple(t), name);
+        } else {
+            static_assert(dependent_bool<T, false>::value, "Invalid Type");
+        }
+    }
+
+    template <typename... Ty>
+    void write(SerializeOutStream &out, const std::tuple<Ty...> &t, const char *name = nullptr)
+    {
+        out.format().beginCompound(out, name);
+        TupleUnpacker::forEach(t, [&](const auto &e) { write(out, e, "Element"); });
+        out.format().endCompound(out, name);
+    }
+
+    template <typename C>
+    void writeContainer(SerializeOutStream &out, const C &container, const char *name = nullptr)
+    {
+        using T = typename C::value_type;
+
+        out.format().beginExtended(out, name);
+        write(out, container.size(), "size");
+        out.format().beginCompound(out, name);
+        for (const auto &t : container) {
+            if (UnitHelper<T>::filter(out, t)) {
+                UnitHelper<T>::beginExtendedItem(out, t);
+                UnitHelper<T>::write_creation(out, t);
+                write(out, t, "Item");
+            }
+        }
+        out.format().endCompound(out, name);
+    }
+
 
 }
 }
