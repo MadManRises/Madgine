f030e881b27fccca4db4f2ca17f7f0bee0c0820c
MadManRises <MadManRises@users.noreply.github.com>
2020-12-11 02:34:16 +0100

    Patch

--- plugins/core/scene/src/Madgine/scene/entity/entitycomponentserialize.h
+++ plugins/core/scene/src/Madgine/scene/entity/entitycomponentserialize.h
@@ -12,12 +12,12 @@ namespace Serialize {
 
         static void read(SerializeInStream &in, std::pair<const uint32_t, Scene::Entity::EntityComponentOwningHandle<Scene::Entity::EntityComponentBase>> &p, const char *name, const CallerHierarchy<Scene::Entity::Entity*> &entity)
         {
-            Serialize::read(in, *entity.mData->sceneMgr().entityComponentList(p.first).get(p.second.mIndex), name, entity);
+            Serialize::read(in, *entity.mData->sceneMgr().entityComponentList(p.first).get(p.second), name, entity);
         }
 
         static void write(SerializeOutStream &out, const std::pair<const uint32_t, Scene::Entity::EntityComponentOwningHandle<Scene::Entity::EntityComponentBase>> &p, const char *name, const CallerHierarchy<const Scene::Entity::Entity*> &entity)
         {
-            Serialize::write(out, *entity.mData->sceneMgr().entityComponentList(p.first).get(p.second.mIndex), name, entity);
+            Serialize::write(out, *entity.mData->sceneMgr().entityComponentList(p.first).get(p.second), name, entity);
         }
     };
 
