a33890be29d78de7fb17c7ea6c353d34beffff2b
Martin Schütz <madmanrises@web.de>
2020-12-18 20:10:11 +0100

    Patch

--- src/Interfaces/dl/runtime.cpp
+++ src/Interfaces/dl/runtime.cpp
@@ -12,7 +12,7 @@ namespace Dl {
 #if LINUX || OSX || IOS || WINDOWS
         return Filesystem::FileQuery { Filesystem::executablePath() };
 #elif ANDROID
-        return SharedLibraryQuery { Filesystem::Path { "/data/data/com.Madgine.MadgineLauncher/lib" } }; //TODO
+        return Filesystem::FileQuery { Filesystem::Path { "/data/data/com.Madgine.MadgineLauncher/lib" } }; //TODO
 #elif EMSCRIPTEN
         std::terminate();
 #else
