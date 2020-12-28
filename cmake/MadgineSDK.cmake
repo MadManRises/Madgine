3d6586f182d6a14b40b2df48c5455fc6d6600009
MadManRises <MadManRises@users.noreply.github.com>
2020-12-29 00:07:51 +0100

    Update launcher naming

--- cmake/MadgineSDK.cmake
+++ cmake/MadgineSDK.cmake
@@ -1,10 +1,6 @@
 
 set(MADGINE_AS_SDK TRUE)
 
-if (NOT MADGINE_BINARY_NAME)
-	MESSAGE(WARNING "When using the Madgine as SDK please provide a 'MADGINE_BINARY_NAME'!")
-	set(MADGINE_BINARY_NAME "MadgineApp")
-endif ()
 
 if (EXISTS ${CMAKE_CURRENT_LIST_DIR}/../CMakeLists.txt)
 #SDK from Source
