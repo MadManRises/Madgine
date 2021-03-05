4f03e361e1b7b6e9a2d26588a64bb132571fb5f6
Martin Schütz <madmanrises@web.de>
2021-03-05 18:50:29 +0100

    Patch

--- src/Generic/compatibility.h
+++ src/Generic/compatibility.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#include <version>
+#ifdef __has_include
+#    if __has_include(<version>)
+#        include <version>
+#    endif
+#endif 
+
 
 #if __cpp_lib_constexpr_algorithms >= 201806L
 #    define CONSTEXPR_ALGORITHM constexpr
 #else
-#define CONSTEXPR_ALGORITHM inline
+#    define CONSTEXPR_ALGORITHM inline
 #endif
