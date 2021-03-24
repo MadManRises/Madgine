b474e3518512e21da2bfbc3bd6fe68ffae1897ff
Martin Schütz <madmanrises@web.de>
2021-03-24 03:57:28 +0100

    Patch

--- src/Meta/keyvalue/function.h
+++ src/Meta/keyvalue/function.h
@@ -6,25 +6,25 @@ namespace Engine {
 
 struct META_EXPORT Function {
     template <typename R, typename... Args, size_t... I>
-    static void unpackHelper(const void *f, ValueType &retVal, const ArgumentList &args, std::index_sequence<I...>)
+    static void unpackHelper(void (*f)(), ValueType &retVal, const ArgumentList &args, std::index_sequence<I...>)
     {
         if constexpr (std::is_same_v<R, void>) {
-            static_cast<R(*)(Args...)>(f)(ValueType_as<std::remove_cv_t<std::remove_reference_t<Args>>>(getArgument(args, I))...);
+            reinterpret_cast<R (*)(Args...)>(f)(ValueType_as<std::remove_cv_t<std::remove_reference_t<Args>>>(getArgument(args, I))...);
             to_ValueType<true>(retVal, std::monostate {});
         } else {
-            to_ValueType<true>(retVal, static_cast<R (*)(Args...)>(f)(ValueType_as<std::remove_cv_t<std::remove_reference_t<Args>>>(getArgument(args, I))...));
+            to_ValueType<true>(retVal, reinterpret_cast<R (*)(Args...)>(f)(ValueType_as<std::remove_cv_t<std::remove_reference_t<Args>>>(getArgument(args, I))...));
         }
     }
 
     template <typename R, typename... Args>
-    static void unpackApiMethod(const void *f, ValueType &retVal, const ArgumentList &args)
+    static void unpackApiMethod(void (*f)(), ValueType &retVal, const ArgumentList &args)
     {
         unpackHelper<R, Args...>(f, retVal, args, std::make_index_sequence<sizeof...(Args)>());
     }
 
     template <typename R, typename... Args>
     Function(R (*f)(Args...))
-        : mFunction(f)
+        : mFunction(reinterpret_cast<void(*)()>(f))
         , mWrapper(&unpackApiMethod<R, Args...>)
     {
     }
@@ -36,8 +36,8 @@ struct META_EXPORT Function {
     void operator()(ValueType &retVal, const ArgumentList &args) const;
 
 private:
-    const void *mFunction;
-    void (*mWrapper)(const void *, ValueType &, const ArgumentList&);
+    void (*mFunction)();
+    void (*mWrapper)(void (*)(), ValueType &, const ArgumentList &);
 };
 
 }
