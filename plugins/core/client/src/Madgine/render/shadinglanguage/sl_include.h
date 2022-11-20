c28d6bb307f74f5a59cec8eb90cc3faae6046375
MadManRises <madmanrises@web.de>
2022-11-20 23:21:40 +0100

    Adding float2 to sl support

--- plugins/core/client/src/Madgine/render/shadinglanguage/sl_include.h
+++ plugins/core/client/src/Madgine/render/shadinglanguage/sl_include.h
@@ -4,6 +4,7 @@
 #define struct struct alignas(16)
 
 #define int2 alignas(8) Engine::Vector2i
+#define float2 alignas(8) Engine::Vector2
 #define float3 alignas(16) Engine::Vector3
 #define float4 alignas(16) Engine::Vector4
 #define float4x4 alignas(16) Engine::Matrix4
