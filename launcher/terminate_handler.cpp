c99f4113804dbb6af610d79eef1554b2b5071f0f
MadManRises <madmanrises@web.de>
2023-10-15 16:14:17 +0200

    Adding signal error handling

--- launcher/terminate_handler.cpp
+++ launcher/terminate_handler.cpp
@@ -1,6 +1,10 @@
 #include "Interfaces/interfaceslib.h"
-#include "Interfaces/debug/stacktrace.h"
+
 #include "Generic/guard.h"
+#include "Interfaces/debug/stacktrace.h"
+
+
+#include <csignal>
 
 #ifndef NDEBUG
 void madgine_terminate_handler()
@@ -16,5 +20,25 @@ void madgine_terminate_handler()
     abort();
 }
 
-static Engine::Guard global { []() { std::set_terminate(&madgine_terminate_handler); } };
+void madgine_signal_handler(int signal) {
+    {
+        std::stringstream cout;
+        cout << "Signal caught: " << signal << " (Madgine - Handler)\n ";
+        cout << "Stack-Trace:\n";
+        for (const Engine::Debug::TraceBack &trace : Engine::Debug::StackTrace<64>::getCurrent(1).calculateReadable())
+            cout << trace.mFunction << " (" << trace.mFile << ": " << trace.mLineNr << ")\n";
+        LOG_FATAL(cout.str());
+    }
+    abort();
+}
+
+static Engine::Guard global { []() {
+    std::set_terminate(&madgine_terminate_handler);
+    std::signal(SIGSEGV, &madgine_signal_handler);
+    std::signal(SIGILL, &madgine_signal_handler);
+    std::signal(SIGABRT, &madgine_signal_handler);
+    std::signal(SIGTERM, &madgine_signal_handler);
+    std::signal(SIGFPE, &madgine_signal_handler);
+    std::signal(SIGINT, &madgine_signal_handler);
+} };
 #endif
