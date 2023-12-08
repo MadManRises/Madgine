9abd752246b39117f5b7f8a856d720c9b9523dd0
MadManRises <madmanrises@web.de>
2023-12-08 15:12:27 +0100

    Adding GPU-based validation to DirectX12

--- plugins/renderer/directx12/src/DirectX12/directx12rendercontext.cpp
+++ plugins/renderer/directx12/src/DirectX12/directx12rendercontext.cpp
@@ -102,6 +102,10 @@ namespace Render {
             ReleasePtr<ID3D12Debug> debugController;
             if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
                 debugController->EnableDebugLayer();
+                ReleasePtr<ID3D12Debug1> debugController1;
+                if (SUCCEEDED(debugController->QueryInterface(IID_PPV_ARGS(&debugController1)))) {
+                    debugController1->SetEnableGPUBasedValidation(true);
+                }
             }
         }
 
