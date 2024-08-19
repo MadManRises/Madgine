b7a82f56d5d757df3c9b99737b296427a2c429f8
MadManRises <madmanrises@web.de>
2024-08-19 23:16:36 +0200

    Fix compile error

--- plugins/core/resources/tools/Madgine_Tools/resourcestoolslib.h
+++ plugins/core/resources/tools/Madgine_Tools/resourcestoolslib.h
@@ -2,8 +2,6 @@
 
 #include "Madgine_Tools/toolslib.h"
 #include "Madgine/resourceslib.h"
-#include "Madgine/audioloaderlib.h"
-#include "Madgine/meshloaderlib.h"
 
 #if defined(ResourcesTools_EXPORTS)
 #    define MADGINE_RESOURCES_TOOLS_EXPORT DLL_EXPORT
